cbb4cbc3499e379c02558a7579a2046f5257e6ef
lilvallo <x_vallo_x@hotmail.com>
2023-11-22 23:47:41 -0600

    primera grid terminada

--- scrips.js
+++ scrips.js
@@ -1,1 +1,17 @@
-grid = document.getElementById
+var grid = document.getElementById("grid");
+var maxGrid = 256
+
+for (var i = 0; i < maxGrid; i++){
+
+    var newDiv = document.createElement("div");
+    newDiv.classList.add("oneGrid");
+    grid.appendChild(newDiv);
+
+    newDiv.addEventListener("mouseover", hooverBlack);
+
+}
+
+function hooverBlack(){
+    var currentDiv = this;
+    currentDiv.style.backgroundColor = "black"
+}
